897b933766aa848044ff937ddcf92e11d4da19ae
rtahboub <rtahboub@purdue.edu>
2016-09-30 12:43:55 -0400

    sync STR_Tree

--- src/test/scala/lms/tutorial/STR_TreeTest.scala
+++ src/test/scala/lms/tutorial/STR_TreeTest.scala
@@ -39,28 +39,51 @@ class STR_TreeTest extends TutorialFunSuite {
             num: Rep[Int] //RInt(num)
           }
 
-          def nextDouble(d: Rep[Char]) = {
+//          def nextDouble(d: Rep[Char]) = {
+//
+//            //println("TODO: implement nextDouble")
+//            // adapt from here:
+//            // https://github.com/TiarkRompf/legobase-micro/blob/master/minidb/src/main/scala/Scanner.scala#L49
+//            val start = pos: Rep[Double] // force read
+//            val first = nextInt('.')
+//            val second = nextInt(d)
+//            var invariant =  second
+//            var denom = 1
+//            //var denom = if (second > 0 ) 1 else 0
+//
+//            first.asInstanceOf[Rep[Double]]
+//            second.asInstanceOf[Rep[Double]]
+//
+////            while (invariant != 0 ){
+////              invariant = invariant /10
+////              denom = denom * 10
+////            }
+//            //val res = if (denom > 0) first.doubleValue + second.doubleValue /denom else first.doubleValue
+//            val res = first.doubleValue
+//            res: Rep[Double]
+//          }
 
-            //println("TODO: implement nextDouble")
-            // adapt from here:
-            // https://github.com/TiarkRompf/legobase-micro/blob/master/minidb/src/main/scala/Scanner.scala#L49
+          def nextDouble(d: Rep[Char] = '|'): Rep[Double] = {
             val start = pos: Rep[Double] // force read
-            val first = nextInt('.')
-            val second = nextInt(d)
-            var invariant =  second
+            var num = 0
             var denom = 1
-
-            first.asInstanceOf[Rep[Double]]
-            second.asInstanceOf[Rep[Double]]
-
-            while (invariant != 0 ){
-              invariant = invariant /10
-              denom = denom * 10
+            while (boolean_and(data(pos) != '.', data(pos) != d) && data(pos) != '\n') {
+              num = num * 10 + (data(pos) - '0').toInt
+              pos += 1
             }
-            val res = if (denom > 0) first.doubleValue + second.doubleValue /denom else first.doubleValue
-            res: Rep[Double]
+            if (data(pos) == '.') {
+              pos += 1
+              while (boolean_and(data(pos) != d, data(pos) != '\n')) {
+                num = num * 10 + (data(pos) - '0').toInt
+                denom = denom * 10
+                pos += 1
+              }
+            }
+            pos += 1
+            val res = num.doubleValue / denom.doubleValue
+            //printf("nextDouble %d/%d=%.2f\n",num,denom,res)
+            res
           }
-
 
           def hasNext = pos < fl
 
@@ -187,21 +210,31 @@ class STR_TreeTest extends TutorialFunSuite {
             s = s / numInLeaf
             numOfLevels += 1
           }
+          numOfLevels = numOfLevels -1
+          //println(numOfLevels)
 
           //helper arrays to assist in finding the start index and end index of each level
           val startLevel: Rep[Array[Int]] = NewArray[Int](numOfLevels)
           val levelActualEnd: Rep[Array[Int]] = NewArray[Int](numOfLevels)
 
-          startLevel(0) = 0
-
-          var v = 1 : Rep[Int]
+          var v = numInLeaf
           var inode_size = 0
-          for (i <- 0 until numOfLevels+1){
+          for (i <- 0 until numOfLevels){
             startLevel(i) = inode_size
             inode_size = inode_size + v
             v = v * numInLeaf
           }
-         // startLevel(numOfLevels-1) = inode_size
+
+          val nexts_size = startLevel(numOfLevels-1) + (size/numInLeaf) + 1
+          println(nexts_size)
+          var nexts: Rep[Array[Int]]    = NewArray[Int](nexts_size)
+          var counts: Rep[Array[Int]]    = NewArray[Int](nexts_size)
+          val stack_size = nexts_size
+          var stack: Rep[Array[Int]]    = NewArray[Int](stack_size)
+          var stack_ptr = 0
+
+          //println(startLevel(numOfLevels-1))
+
 
           var min0:    Rep[Array[Double]] = NewArray[Double](inode_size)
           var max0:    Rep[Array[Double]] = NewArray[Double](inode_size)
@@ -212,6 +245,10 @@ class STR_TreeTest extends TutorialFunSuite {
             keys(numPoints) = p.key
             xs(numPoints) = p.x
             ys(numPoints) = p.y
+
+//            println(xs(numPoints))
+//            println(ys(numPoints))
+//            println("-----")
             numPoints += 1
           }
 
@@ -221,7 +258,7 @@ class STR_TreeTest extends TutorialFunSuite {
             //var currentLevel = numOfLevels - 1
             //var currentSartIndex = inode_size - power(currentLevel)
 
-            var currentSartIndex = startLevel(numOfLevels - 1)
+            var currentSartIndex = startLevel(numOfLevels-1)
 
             var k = 0 //the starting index of xs and ys
             while(k<size-numInLeaf){
@@ -246,6 +283,13 @@ class STR_TreeTest extends TutorialFunSuite {
               min1(currentSartIndex) = min1Val
               max1(currentSartIndex) = max1Val
 
+              nexts(currentSartIndex) = k
+              counts(currentSartIndex) = numInLeaf
+//              println(currentSartIndex)
+//              println("---")
+//              println(k)
+
+
               currentSartIndex = currentSartIndex + 1
 
               k = k + numInLeaf
@@ -253,6 +297,7 @@ class STR_TreeTest extends TutorialFunSuite {
             }
 
             if(k < size){
+              var c = 0
               //last leaf is incomplete
               val remaining = size-k
 
@@ -270,11 +315,17 @@ class STR_TreeTest extends TutorialFunSuite {
                   min1Val = ys(i)
                 if(ys(i) > max1Val)
                   max1Val = ys(i)
+
+                c = c+1
               }
               min0(currentSartIndex) = min0Val
               max0(currentSartIndex) = max0Val
               min1(currentSartIndex) = min1Val
               max1(currentSartIndex) = max1Val
+
+              nexts(currentSartIndex) = k
+              counts(currentSartIndex) = c
+
             }
             else
               currentSartIndex = currentSartIndex - 1
@@ -284,17 +335,13 @@ class STR_TreeTest extends TutorialFunSuite {
 
             //println(currentSartIndex)
 
-            var j = numOfLevels-2
+            var j = numOfLevels-1
 
+            while(j>0) {
+              currentSartIndex = startLevel(j-1)
+              k = startLevel(j) //the start index of the level we are going to process
 
-            while(j>=0) {
-              currentSartIndex = startLevel(j)
-              k = startLevel(j+1) //the start index of the level we are going to process
-            println(currentSartIndex)
-            println(k)
-            println(levelActualEnd(j+1))
-
-              while(k < levelActualEnd(j+1) - numInLeaf){
+              while(k < levelActualEnd(j) - numInLeaf){
 
                 var min0Val = min0(k): Rep[Double]
                 var max0Val = max0(k): Rep[Double]
@@ -317,16 +364,19 @@ class STR_TreeTest extends TutorialFunSuite {
                 min1(currentSartIndex) = min1Val
                 max1(currentSartIndex) = max1Val
 
+                nexts(currentSartIndex) = k
+                counts(currentSartIndex) = numInLeaf
+
                 currentSartIndex = currentSartIndex + 1
 
                 k = k + numInLeaf
 
               }
 
-            println(k)
-            if(k < levelActualEnd(j+1)){ //last node is incomplete
-              val remaining = levelActualEnd(j+1) - k
-              println(remaining)
+            if(k < levelActualEnd(j)){ //last node is incomplete
+              var c2 = 0
+              val remaining = levelActualEnd(j) - k
+              //println(remaining)
 
               var min0Val = min0(k): Rep[Double]
               var max0Val = max0(k): Rep[Double]
@@ -334,6 +384,9 @@ class STR_TreeTest extends TutorialFunSuite {
               var max1Val = max1(k): Rep[Double]
 
               for(i<- 1 + k until remaining +k){
+
+                c2 = c2 + 1
+
                 if(min0(i) < min0Val)
                   min0Val = min0(i)
                 if(max0(i) > max0Val)
@@ -349,29 +402,75 @@ class STR_TreeTest extends TutorialFunSuite {
               min1(currentSartIndex) = min1Val
               max1(currentSartIndex) = max1Val
 
+              nexts(currentSartIndex) = k
+              counts(currentSartIndex) = c2
+
             }
-            else
+            else{
               currentSartIndex = currentSartIndex - 1
+            }
 
-            println(currentSartIndex)
-            levelActualEnd(j) = currentSartIndex
+            levelActualEnd(j-1) = currentSartIndex
 
-              println(j)
               j = j-1
             }
-
-            println(min0(0))
-            println(max0(0))
-            println(min1(0))
-            println(max1(0))
-
-
-
-
-
-
+//sbt -J-Xmx3G -J-Xms1G -J-XX:MaxPermSize=1G
+//            println(min0(0))
+//            println(max0(0))
+//            println(min1(0))
+//            println(max1(0))
+//
+//            for(i<- 0 until nexts_size) {
+//              print(i)
+//              print("---")
+//              println(counts(i))
+//            }
 
           }
+
+          def printIndex(key_id: Rep[Int]) : Rep[Unit] = {
+            //push the root in the stack
+            var k = counts(key_id) - 1
+            while (k>=0){
+              stack(stack_ptr) = k
+              stack_ptr = stack_ptr + 1
+              k = k - 1
+            }
+
+//            for (i<-0 until nexts_size)
+//              println(counts(i))
+
+            while(stack_ptr != 0){
+              var top = stack(stack_ptr-1)
+              stack_ptr = stack_ptr - 1
+              println(top)
+              var n = nexts(top)
+              if (n < 155 ){
+                if(counts(n) <= 12)
+                  
+                for (i<- 0 until counts(n)){
+
+                  if(stack_ptr < stack_size) {
+                    stack(stack_ptr) = n + i
+                    stack_ptr = stack_ptr + 1
+                  }
+                }
+
+                else
+                  println("count too large")
+
+              }
+              else
+                println("leaf")
+            }
+
+          }
+
+          def printStack(): Rep[Unit]={
+            for (i<- 0 until stack_ptr)
+              println(stack(i))
+          }
+
 
           def power(v: Rep[Int]): Rep[Int]={
             var out = 1: Rep[Int]
@@ -389,9 +488,8 @@ class STR_TreeTest extends TutorialFunSuite {
           val in = new Scanner(filename)
           while (in.hasNext) {
             val id   = in.nextInt(',')
-            val x   = in.nextDouble(',')
-            val y  = in.nextDouble('\n')
-
+            val x   = in.nextInt(',')
+            val y  = in.nextInt('\n')
             f(Point(id,x,y))
           }
           in.done
@@ -400,11 +498,13 @@ class STR_TreeTest extends TutorialFunSuite {
         // main function - code generated from everything inside gets compiled
         def snippet(x: Rep[String]): Rep[Unit] = {
           println("start building")
-          val strTree = new STR_Tree(20000)
-          processCSV("/Users/postgresuser/Research/tutorials/src/data/t2_20000.csv") { p2 =>
+          val strTree = new STR_Tree(20004)
+          processCSV("/Users/postgresuser/Research/tutorials/src/data/t2_20004_correct.csv") { p2 =>
             strTree.addPoint(p2)
           }
           strTree.buildIndex
+          strTree.printIndex(0)
+          //strTree.printStack
           println("end building")
 //          println("start query")
 //          processCSV("/Users/postgresuser/Research/tutorials/src/data/t1_10000.csv") { p1 =>
